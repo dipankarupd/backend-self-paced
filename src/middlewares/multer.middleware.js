634b6eef1164a694981c7b0c60e4b3392a25de31
Dipankar Upadhyaya <drupd17@gmail.com>
2024-03-21 11:05:34 +0530

    file upload config

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,25 @@
+import multer from "multer";
+
+// from the doc:
+// link: https://github.com/expressjs/multer
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './public/temp')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+  })
+  
+  export const upload = multer({ storage: storage })
+
+
+  /*
+  In summary, when a file is uploaded through an HTTP request to the server, 
+  Multer middleware stores it temporarily on the local server in the ./public/temp directory. 
+  Then, the uploadOnCloud function from cloudinary.service.js is called with the path of the 
+  temporarily stored file. This function attempts to upload the file to Cloudinary. If successful, 
+  it returns the Cloudinary response; otherwise, it cleans up the local server by removing the 
+  temporarily stored file and returns null.
+  */
---
+++ src/utils/cloudnary.service.js
@@ -0,0 +1,41 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from "fs"
+
+
+// cloudinary configuration:
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.API_KEY, 
+  api_secret: process.env.API_SECRET_KEY,  
+});
+
+
+// file uploading:
+
+const uploadOnCloud = async (localPath) => {
+    try {
+
+        if (!localPath) {
+            return null
+        }
+
+        // upload on cloudinary
+        const response = await cloudinary.uploader.upload(localPath, {
+            resource_type: "auto"
+        })
+        // file uploaded successfully:
+        console.log("File uploaded successfully on cloud ", response.url);
+
+        return response
+
+    }
+    catch(e) {
+        // if the upload failed, the local file should be removed from the server
+        // to prevent load on the server and prevent the malicious file in there
+        // to remove the file, we do not delete it, we will unlink the given file from our fs
+        fs.unlinkSync(localPath)
+        return null
+    }
+}
+
+export {uploadOnCloud}
